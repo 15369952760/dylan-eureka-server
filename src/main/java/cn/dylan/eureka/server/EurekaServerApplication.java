02a34eb6bdc06f438f922143ecee3afd7f20bd41
yuandehong <12345678>
2018-11-29 15:00:32 +0800

    初始化提交服务注册

---
+++ src/main/java/cn/dylan/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package cn.dylan.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: yuandh
+ * @Description: 注册机启动
+ * @Date: Created in 14:04 2018/11/28
+ * @Modified By:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
